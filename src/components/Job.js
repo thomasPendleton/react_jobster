c52322624953946b40b6edc8a6abcb7ccf6aad6b
Thomas Pendleton <thomas6576@yahoo.com>
2022-11-06 22:00:53 -0800

    add to job component

--- src/components/Job.js
+++ src/components/Job.js
@@ -19,7 +19,22 @@ const Job = ({
   return <Wrapper>
     <header>
       <div className="main-icon">{company.charAt(0)}</div>
+      <div className="info">
+        <h5>{position}</h5>
+        <p>{company}</p>
+      </div>
     </header>
+    <div className="content">
+      <div className="content-center">
+        <h4>more content</h4>
+        <div className={`status ${status}`}>{status}</div>
+      </div>
+      <footer>
+        <div className="actions">
+          <Link to='/add-job' className="btn edit-btn" onClick={() => console.log('edit btn')}>Edit</Link>
+        </div>
+      </footer>
+    </div>
   </Wrapper>
 
 
